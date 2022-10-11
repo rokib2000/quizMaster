bb9a2de4128b5391d3a6ed4784ac36313d0f89c3
rokib2000 <rokib0w1@gmail.com>
2022-10-11 14:14:43 +0600

    added Blog page

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
 
 
   return (
-    <div className="App">
+    <div >
       <RouterProvider router={router}></RouterProvider>
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,23 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { NavLink } from 'react-router-dom';
+import Logo from '../../image/logo.png'
 
 const Header = () => {
     return (
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
         <Container>
-            <Navbar.Brand href="/">quizMaster</Navbar.Brand>
+            <Navbar.Brand href="/">
+            <img
+              alt=""
+              src={Logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            quizMaster
+          </Navbar.Brand>
+            {/* <Navbar.Brand href="/">quizMaster</Navbar.Brand> */}
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="ms-auto">
