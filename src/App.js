8fa12c69257ef6b8ef9bc56237685d5c88f8fe4e
rokib2000 <rokib0w1@gmail.com>
2022-10-11 13:48:44 +0600

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 
 import {  createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Blog from './components/Blog/Blog';
 import Home from './components/Home/Home';
+import Statistics from './components/Statistics/Statistics';
 import Main from './layouts/Main';
 
 function App() {
@@ -13,6 +15,14 @@ function App() {
         {
           path: '/',
           element: <Home></Home>
+        },
+        {
+          path: 'statistics',
+          element: <Statistics></Statistics>
+        },
+        {
+          path: 'blog',
+          element: <Blog></Blog>
         }
       ]
     }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <h1>Header </h1>
-        </div>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+            <Navbar.Brand href="/">quizMaster</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="ms-auto">
+                    <NavLink className='nav-link' to='/'>Home</NavLink>
+                    <NavLink className='nav-link' to='/statistics'>Statistics</NavLink>
+                    <NavLink className='nav-link' to='/blog'>Blog</NavLink>
+                </Nav>
+            </Navbar.Collapse>
+        </Container>
+        </Navbar>
     );
 };
 
