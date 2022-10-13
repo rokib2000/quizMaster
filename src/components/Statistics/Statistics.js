556e75fb80e77900503e2e270250b2f5d6e24e32
rokib2000 <rokib0w1@gmail.com>
2022-10-13 11:31:57 +0600

    fix rechart responsive

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import { useLoaderData } from "react-router-dom";
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
-import "./Statistics.css";
 
 const Statistics = () => {
   const quiz = useLoaderData();
@@ -12,8 +11,8 @@ const Statistics = () => {
   return (
     <div className="my-4 ">
       <Container>
-        <ResponsiveContainer className="rechart">
-          <LineChart width={1000} height={600} data={data}>
+        <ResponsiveContainer width={"100%"} aspect={2}>
+          <LineChart data={data}>
             <Line type="monotone" dataKey="total" stroke="#8884d8" />
             <CartesianGrid stroke="#ccc" />
             <XAxis dataKey="name" />
