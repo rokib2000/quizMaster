02fc3b5cffcc9106eb22acf953091d502f5574d2
rokib2000 <rokib0w1@gmail.com>
2022-10-13 11:00:42 +0600

    responsive rechart

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import { useLoaderData } from "react-router-dom";
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
+import "./Statistics.css";
 
 const Statistics = () => {
   const quiz = useLoaderData();
@@ -11,14 +12,16 @@ const Statistics = () => {
   return (
     <div className="my-4 ">
       <Container>
-        <LineChart width={1000} height={600} data={data}>
-          <Line type="monotone" dataKey="total" stroke="#8884d8" />
-          <CartesianGrid stroke="#ccc" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-        </LineChart>
+        <ResponsiveContainer className="rechart">
+          <LineChart width={1000} height={600} data={data}>
+            <Line type="monotone" dataKey="total" stroke="#8884d8" />
+            <CartesianGrid stroke="#ccc" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+          </LineChart>
+        </ResponsiveContainer>
       </Container>
     </div>
   );
